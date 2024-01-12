6c890d44cb902250ff9da8e4f6ebe7d22ffb3bc9
Bowie <lvillon@coppe.ufrj.br>
2024-01-12 14:12:12 -0300

    second commit

--- main.py
+++ main.py
@@ -4,17 +4,9 @@ Autor: Luis Villon
 """
 
 
-# import subprocess
-# import os
-# from contextlib import chdir
-
-
 def main() -> None:
-    # os.chdir("/media/luis/sdc3-arq-ubu/GITHUB-PROJECT/Visualizacoes-análise-exploratória")
     with open("analysis.py") as f:
         exec(f.read())
-    # subprocess.run(["python", "src/analysis.py"])
-    # os.system(f'python {"src/analysis.py"}')
 
 
 if __name__ == '__main__':
